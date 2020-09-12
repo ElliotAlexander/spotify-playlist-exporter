cd33b0f4720e735a586fad1ce847f24e2cb74446
Elliot Alexander <ep1e16@soton.ac.uk>
2020-09-12 13:25:40 +0100

    Formatting fixes in Auth.go

--- Auth.go
+++ Auth.go
@@ -1,42 +1,35 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-    "encoding/json"
-	"github.com/zmb3/spotify"
+    "fmt"
+    "log"
+    "net/http"
+    "github.com/zmb3/spotify"
 )
 
-func handleCallback(client *spotify.Client) {
-	user, err := client.CurrentUser()
-	if err != nil {
-		log.Fatal(err)
-	}
+func handleCallback(client *spotify.Client)  (playlistArr []spotify.SimplePlaylist){
+    user, err := client.CurrentUser()
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	fmt.Println("You are logged in as:", user.ID)
-	simplePlaylistPages := *retrievePlaylistIDs(client)
-	playlists := simplePlaylistPages.Playlists
-	jsonData, err := json.Marshal(playlists)
-	if err != nil {
-	   log.Println(err)
-	}
-	fmt.Println(string(jsonData))
+    fmt.Println("You are logged in as:", user.ID)
+    playlistArr = retrievePaginatedPlaylists(client)
     return
 }
 
 func completeAuth(w http.ResponseWriter, r *http.Request) {
-	tok, err := auth.Token(state, r)
-	if err != nil {
-		http.Error(w, "Couldn't get token", http.StatusForbidden)
-		log.Fatal(err)
-	}
-	if st := r.FormValue("state"); st != state {
-		http.NotFound(w, r)
-		log.Fatalf("State mismatch: %s != %s\n", st, state)
-	}
-	// use the token to get an authenticated client
-	client := auth.NewClient(tok)
-	fmt.Fprintf(w, "Login Completed!")
-	ch <- &client
+    tok, err := auth.Token(state, r)
+    if err != nil {
+        http.Error(w, "Couldn't get token", http.StatusForbidden)
+        log.Fatal(err)
+    }
+    if st := r.FormValue("state"); st != state {
+        http.NotFound(w, r)
+        log.Fatalf("State mismatch: %s != %s\n", st, state)
+    }
+    // use the token to get an authenticated client
+    client := auth.NewClient(tok)
+    fmt.Fprintf(w, "Login Completed!")
+    ch <- &client
 }
