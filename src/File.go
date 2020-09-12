bec0bdf29728db00be3ec853f269ed79ed448c58
ElliotAlexander <elliot.alex.97@gmail.com>
2020-09-12 23:48:55 +0100

    Add method to strip forward slashes from filepaths

--- src/File.go
+++ src/File.go
@@ -2,11 +2,14 @@ package main
 
 import (
     "io/ioutil"
+    "strings"
     "os"
 )
 
-func writeToFile(filename string, data []byte){
-    err := ioutil.WriteFile(filename, data, 0644)
+func writeToFile(path string, filename string, data []byte) {
+    // Playlists with forward slashes in their name will break without this.
+    filepath := path + stripSlashes(filename)
+    err := ioutil.WriteFile(filepath, data, 0644)
     if err != nil {
         panic(err)
     }
@@ -21,3 +24,7 @@ func fileExists(filename string) bool {
     return !info.IsDir()
 }
 
+func stripSlashes(filename string) (filenameStripped string){
+    filenameStripped = strings.Replace(filename, "/", "", -1)
+    return
+}
