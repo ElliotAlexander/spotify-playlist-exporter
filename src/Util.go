93803bea366aa4bfe03e130dd1308f7887f537d5
ElliotAlexander <elliot.alex.97@gmail.com>
2020-09-13 13:19:58 +0100

    Fix formatting in Util.go

--- src/Util.go
+++ src/Util.go
@@ -9,21 +9,21 @@ import (
 )
 
 func openBrowser(url string) {
-	var err error
+    var err error
 
-	switch runtime.GOOS {
-	case "linux":
-		err = exec.Command("xdg-open", url).Start()
-	case "windows":
-		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
-	case "darwin":
-		err = exec.Command("open", url).Start()
-	default:
-		err = fmt.Errorf("unsupported platform")
-	}
-	if err != nil {
+    switch runtime.GOOS {
+    case "linux":
+        err = exec.Command("xdg-open", url).Start()
+    case "windows":
+        err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+    case "darwin":
+        err = exec.Command("open", url).Start()
+    default:
+        err = fmt.Errorf("unsupported platform")
+    }
+    if err != nil {
         log.Fatal("Failed to login: Please open the following URL in your browser. " + url)
-	}
+    }
 }
 
 func dumpToJson(value interface{}) (jsonData []byte) {
